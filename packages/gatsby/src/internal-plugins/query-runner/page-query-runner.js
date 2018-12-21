jawebgit/gatsby

commit 16b01b65e7bc5f5c7b6835cc8afc53f4e13e3c15

fix(gatsby): fix eventemitter leak in page query runner (#10593)

packages/gatsby/src/internal-plugins/query-runner/page-query-runner.js
@@ -162,9 +162,11 @@ const runQueriesForPathnames = pathnames => {
   }
 
   return new Promise(resolve => {
-    queue.on(`drain`, () => {
+    const onDrain = () => {
+      queue.off(`drain`, onDrain)
       resolve()
-    })
+    }
+    queue.on(`drain`, onDrain)
   })
 }
 

commit 006f514a00503652a1e986236f26efa96cefb244

fix(query-runner): replace .off with .removeListener (#10613)
<!--
  Have any questions? Check out the contributing docs at https://gatsby.app/contribute, or
  ask in this Pull Request and a Gatsby maintainer will be happy to help :)
-->

## Description

<!-- Write a brief description of the changes introduced by this PR -->
This fixes a bug introduced in #10593 by replacing the `.off` call with `.removeListener`. `.off` was introduced in Node v10.0.0 as an alias for `.removeListener` (https://github.com/nodejs/node/pull/17156).

## Related Issues

<!--
  Link to the issue that is fixed by this PR (if there is one)
  e.g. Fixes #1234, Addresses #1234, Related to #1234, etc.
-->
Related to #10612

packages/gatsby/src/internal-plugins/query-runner/page-query-runner.js
@@ -163,7 +163,7 @@ const runQueriesForPathnames = pathnames => {
 
   return new Promise(resolve => {
     const onDrain = () => {
-      queue.off(`drain`, onDrain)
+      queue.removeListener(`drain`, onDrain)
       resolve()
     }
     queue.on(`drain`, onDrain)